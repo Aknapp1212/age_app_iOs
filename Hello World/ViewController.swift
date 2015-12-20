72a4d1b46c0827810079414ab0a7c518add714ca
Adrian Knapp <Aknapp1212@gmail.com>
2015-12-20 03:37:03 -0500

    added submit button functionality, when user taps on submit, label changes to user data from text field

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Age_label: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    
+    @IBAction func Submit(sender: AnyObject) {
+        print("Submit button Tapped")
+        Age_label.text = textField.text
+    }
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         
         //console log to prove app is loading
         print("Hello World!")
         
     }
+    
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
